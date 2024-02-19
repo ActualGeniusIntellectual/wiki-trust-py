61dd14205fc614af31cff67de3db4ba4eba80181
Actual Genius Intellectual <math.cs.researcher@gmail.com>
2024-02-19 15:53:56 -0800

    Refactor revision fetching and storage

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import logging
 import requests
 import json
-import os
-import time
 import sqlite3
 
-# Set up logging. Include the time, level, and message
+# Set up logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Constants
 WIKI_API_URL = "https://en.wikipedia.org/w/api.php"
-PAGE_TITLE = "Python_(programming_language)"  # Example Wikipedia page
+PAGE_TITLES = ["Python_(programming_language)", "Java_(programming_language)", "C++"]  # Example list of Wikipedia pages
 
 # Database setup
 conn = sqlite3.connect('revisions.db')
@@ -26,14 +24,13 @@ cursor.execute('''
         user TEXT
     )
 ''')
-
 conn.commit()
 
-def get_revision_count():
+def get_revision_count(page_title):
     params = {
         'action': 'query',
         'format': 'json',
-        'titles': PAGE_TITLE,
+        'titles': page_title,
         'prop': 'revisions',
         'rvprop': 'ids',
         'rvlimit': '1'
@@ -44,49 +41,48 @@ def get_revision_count():
     return len(data['query']['pages'][page_id]['revisions'])
 
 def fetch_and_store_revisions():
-    stored_revisions_count = cursor.execute('SELECT COUNT(*) FROM revisions').fetchone()[0]
-    # Log the number of stored revisions
-    logging.info(f'Number of stored revisions: {stored_revisions_count}')
+    for page_title in PAGE_TITLES:
+        stored_revisions_count = cursor.execute('SELECT COUNT(*) FROM revisions WHERE page = ?', (page_title,)).fetchone()[0]
+        logging.info(f'Number of stored revisions for {page_title}: {stored_revisions_count}')
 
-    api_revisions_count = get_revision_count()
+        api_revisions_count = get_revision_count(page_title)
 
-    if stored_revisions_count < api_revisions_count:
-        params = {
-            'action': 'query',
-            'format': 'json',
-            'titles': PAGE_TITLE,
-            'prop': 'revisions',
-            'rvprop': 'ids|timestamp|size|flags|comment|user',
-            'rvlimit': '500'  # Adjust as needed, max is 500 for non-bots
-        }
+        if stored_revisions_count < api_revisions_count:
+            params = {
+                'action': 'query',
+                'format': 'json',
+                'titles': page_title,
+                'prop': 'revisions',
+                'rvprop': 'ids|timestamp|size|flags|comment|user',
+                'rvlimit': '500'
+            }
 
-        while True:
-            response = requests.get(WIKI_API_URL, params=params)
-            data = response.json()
-            page_id = next(iter(data['query']['pages']))
-            revisions = data['query']['pages'][page_id]['revisions']
+            while True:
+                response = requests.get(WIKI_API_URL, params=params)
+                data = response.json()
+                page_id = next(iter(data['query']['pages']))
+                revisions = data['query']['pages'][page_id]['revisions']
 
-            # Insert revisions into the database
-            for rev in revisions:
-                cursor.execute('''
-                    INSERT OR IGNORE INTO revisions (id, page, timestamp, minor, size, comment, user)
-                    VALUES (?, ?, ?, ?, ?, ?, ?)
-                ''', (
-                    rev['revid'],
-                    PAGE_TITLE,
-                    rev.get('timestamp'),
-                    'minor' in rev,  # True if 'minor' flag exists
-                    rev.get('size'),
-                    rev.get('comment'),
-                    rev.get('user')
-                ))
-            
-            conn.commit()
+                for rev in revisions:
+                    cursor.execute('''
+                        INSERT OR IGNORE INTO revisions (id, page, timestamp, minor, size, comment, user)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                    ''', (
+                        rev['revid'],
+                        page_title,
+                        rev.get('timestamp'),
+                        'minor' in rev,
+                        rev.get('size'),
+                        rev.get('comment'),
+                        rev.get('user')
+                    ))
 
-            if 'continue' in data:
-                params['rvcontinue'] = data['continue']['rvcontinue']
-            else:
-                break
+                conn.commit()
+
+                if 'continue' in data:
+                    params['rvcontinue'] = data['continue']['rvcontinue']
+                else:
+                    break
 
 if __name__ == '__main__':
     fetch_and_store_revisions()
